8d2a677e97388c56006e71653bb312e187cc0725
Saurabh Bhandari <bhandarisaurabh143@gmail.com>
2023-04-02 11:26:06 +0530

    pagination update

--- frontend/src/pages/Products/Pagination.jsx
+++ frontend/src/pages/Products/Pagination.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
 function Pagination({current,onChange}) {
-    const prev = <button data-testid="prev-page" disabled={current===1} 
-    onClick={()=>onChange(current-1)}>PREV</button>;
-    const currentPage = <button data-testid="current-page">{current}</button>;
-    const next = <button data-testid="next-page" onClick={()=>onChange(current+1)}>NEXT</button>;
+    const prev = <Button disabled={current===1} 
+    onClick={()=>onChange(current-1)}>PREV</Button>;
+    const currentPage = <Button >{current}</Button>;
+    const next = <Button  onClick={()=>onChange(current+1)}>NEXT</Button>;
     return (
       <div style={{display:'flex',marginTop:'20px',gap:'10px',justifyContent:'center',alignItems:'center'}}>
         {prev}
