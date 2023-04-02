24cf4510b8a286ca78aed3afc6d35cf9346aef95
abhi-swami <manjuswami338@gmail.com>
2023-04-02 18:58:27 +0530

    updated some more

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Footer3 from "./Footer3";
+import Footer4 from "./Footer4";
 
 export default function Footer() {
   return (
     <div>
       <Footer3 />
-      {/* <Footer4 /> */}
-      {/* <Footer5 /> */}
+      <Footer4 />
     </div>
   );
 }
